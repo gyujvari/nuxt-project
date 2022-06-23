b6fe5ad394fc21fbd99fc444610a6d3beb7f5978
gyorgy <ujvari.gyorgy@gmail.com>
2022-06-23 10:45:07 +0300

    login component

--- components/Login.vue
+++ components/Login.vue
@@ -1,11 +1,39 @@
-<template>
-  <div>
-   
-  </div>
-</template>
 
+<template>
+    <div>
+         <input type="text" v-model="credentials.user" placeholder="email"/>
+         <input type="text" v-model="credentials.password" placeholder="email"/>
+         <button @click="test">clickme</button>
+         <button @click="check">gettertest</button>
+         <div v-for="p in this.$store.state.data" :key="p.id">
+            {{ p.text }}
+          </div>
+          <tbody>
+                <tr v-for="item in this.$store.state.data" :key="item.id">
+                    <td>{{item.text}} {{item.id}}</td>
+                </tr>
+            </tbody>
+    </div>
+</template>
 <script>
-export default {
-  name: 'Login'
-}
+    export default{
+        name: 'Login',
+        data () {
+        
+            return{
+                 credentials:{
+                    user:'',
+                    password:'',
+                },
+            }
+
+        },
+        methods: {
+           
+            Login() {
+                this.$store.dispatch("fetchSomething", this.credentials)
+            },
+        }
+    }
+
 </script>
